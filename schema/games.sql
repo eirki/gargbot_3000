946059886d91448ed8bd747e2e7a673615be1440
eirki <eirik.b.stavest@gmail.com>
2018-03-02 09:25:52 +0100

    typo

--- schema/games.sql
+++ schema/games.sql
@@ -9,11 +9,11 @@ CREATE TABLE games_votes (
 game_id mediumint(8) ,
 slack_id CHAR(9)
 );
-ALTER TABLE games_votes ADD CONSTRAINT constraintname UNIQUE (game_id, slack_id);
+ALTER TABLE games_votes ADD CONSTRAINT UNIQUE (game_id, slack_id);
 
 DROP TABLE IF EXISTS games_stars;
 CREATE TABLE games_stars (
 game_id mediumint(8) ,
 slack_id CHAR(9)
 );
-ALTER TABLE games_stars ADD CONSTRAINT constraintname UNIQUE (game_id, slack_id);
+ALTER TABLE games_stars ADD CONSTRAINT UNIQUE (game_id, slack_id);
