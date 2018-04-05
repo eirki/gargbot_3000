7b13cb29ef6c3af074513d7b8c8a7f4c4c5d4e2a
eirki <eirik.b.stavest@gmail.com>
2018-04-05 15:39:51 +0200

    add bot server function

--- gargbot_3000/server.py
+++ gargbot_3000/server.py
@@ -2,23 +2,72 @@
 # coding: utf-8
 from gargbot_3000.logger import log
 
-from flask import Flask
+from flask import Flask, request, g
 
-from gargbot_3000 import config
+from gargbot_3000 import commands
+from gargbot_3000 import database_manager
+from gargbot_3000 import quotes
+from gargbot_3000 import droppics
 
 app = Flask(__name__)
 
+
+def get_db():
+    db_connection = getattr(g, '_database', None)
+    if db_connection is None:
+        db_connection = database_manager.connect_to_database()
+        g._database = db_connection
+    return db_connection
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db_connection = getattr(g, '_database', None)
+    if db_connection is not None:
+        db_connection.close()
+
+
 @app.route('/')
 def hello_world() -> str:
-    return f"{config.app_id}.home"
+    return "home"
 
 
-@app.route('/gargbot_3000')
-def gargbot_3000():
-    return "gargbot_3000"
+@app.route('/slash_cmds')
+def slash_cmds():
+    data = request.form
+    command_str = data["command"][1:]
+    args = data['text']
+    try:
+        command_function = commands.command_switch[command_str]
+    except KeyError:
+        command_function = commands.cmd_not_found
+        args = [command_str]
+
+    if command_str in {"msn", "quote", "pic"}:
+        db_connection = get_db()
+        command_function.keywords["db"] = db_connection
+
+    response = commands.try_or_panic(command_function, *args)
+
+    return response
+
+    if response is None:
+        return ""
+
+
+def setup():
+    quotes_db = quotes.Quotes(db=get_db())
+    commands.command_switch["msn"].keywords["quotes_db"] = quotes_db
+    commands.command_switch["quote"].keywords["quotes_db"] = quotes_db
+
+    drop_pics = droppics.DropPics(db=get_db())
+    drop_pics.connect_dbx()
+    commands.command_switch["pic"].keywords["drop_pics"] = drop_pics
 
 
 def main():
+    setup()
+    log.info("GargBot 3000 server operational!")
     # app.run() uwsgi does this
     pass
 
