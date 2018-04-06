8baf6acec249dcc0bd10a8782351b27cfa355a7d
eirki <eirik.b.stavest@gmail.com>
2018-04-06 13:24:48 +0200

    move duplicate code into separate function

--- gargbot_3000/server.py
+++ gargbot_3000/server.py
@@ -55,7 +55,7 @@ def attach_buttons(callback_id, result, func, args):
     ]
     result["attachments"][0]["actions"] = actions
     result["attachments"][0]["callback_id"] = callback_id
-
+    return result
 
 @app.route('/')
 def hello_world() -> str:
@@ -70,48 +70,28 @@ def interactive():
     if not data.get('token') == config.v2_verification_token:
         return Response(status=403)
     action = data["actions"][0]["name"]
+
     if action == "Send":
         log.info("Interactive: Send")
         result = json.loads(data["actions"][0]["value"])["original_response"]
         result["response_type"] = "in_channel"
+        return result
 
     elif action == "Avbryt":
         log.info("Interactive: Avbryt")
         result = {
             "response_type": "ephemeral",
-            "replace_original": True,
             "text": "Canceled! Går fint det. Ikke noe problem for meg. Kødd."
         }
 
     elif action == "Shuffle":
         log.info("Interactive: Shuffle")
-        callback_id = data["callback_id"]
         command_str = json.loads(data["actions"][0]["value"])["original_func"]
         args = json.loads(data["actions"][0]["value"])["original_args"]
+        callback_id = data["callback_id"]
+        result = get_and_execute_command(command_str, args, callback_id)
 
-        # duplicate code follows
-        log.info(f"command: {command_str}")
-        log.info(f"args: {args}")
-
-        try:
-            command_function = commands.command_switch[command_str]
-        except KeyError:
-            command_function = commands.cmd_not_found
-            args = [command_str]
-
-        if command_str in {"msn", "quote", "pic"}:
-            db_connection = get_db()
-            command_function.keywords["db"] = db_connection
-
-        result = commands.try_or_panic(command_function, args)
-        if not result.get("text", "").startswith("Error"):
-            attach_buttons(
-                callback_id=callback_id,
-                result=result,
-                func=command_str,
-                args=args
-            )
-            result["response_type"] = "ephemeral"
+    result["replace_original"] = True
     log.info(f"result: {result}")
     return Response(
         response=json.dumps(result),
@@ -125,12 +105,25 @@ def slash_cmds():
     log.info("incoming slash request:")
     data = request.form
     log.info(data)
-    trigger_id = data["trigger_id"]
 
     if not data.get('token') == config.v2_verification_token:
         return Response(status=403)
+
     command_str = data["command"][1:]
     args = data['text'].split()
+    trigger_id = data["trigger_id"]
+
+    result = get_and_execute_command(command_str, args, trigger_id)
+
+    log.info(f"result: {result}")
+    return Response(
+        response=json.dumps(result),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+def get_and_execute_command(command_str, args, callback_id):
     log.info(f"command: {command_str}")
     log.info(f"args: {args}")
 
@@ -146,18 +139,14 @@ def slash_cmds():
 
     result = commands.try_or_panic(command_function, args)
     result["response_type"] = "ephemeral"
-    attach_buttons(
-        callback_id=trigger_id,
-        result=result,
-        func=command_str,
-        args=args
-    )
-    log.info(f"result: {result}")
-    return Response(
-        response=json.dumps(result),
-        status=200,
-        mimetype='application/json'
-    )
+    if not result.get("text", "").startswith("Error"):
+        result = attach_buttons(
+            callback_id=callback_id,
+            result=result,
+            func=command_str,
+            args=args
+        )
+    return result
 
 
 def setup():
