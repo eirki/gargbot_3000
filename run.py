27ddfc4b3f2a0343d63595a8e9f79df05d0dbc43
eirki <eirik.b.stavest@gmail.com>
2018-08-23 13:18:01 +0200

    Cosmetic changes

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ lock_socket = None  # we want to keep the socket open until the very end of
                     # our script so we use a global variable to avoid going
                     # out of scope and being garbage-collected
 
+
 def is_lock_free():
     global lock_socket
     lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
