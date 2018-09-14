2b389a3ebe95fb1717b9806def71926b9c3fab18
eirki <eirik.b.stavest@gmail.com>
2018-09-14 09:47:09 +0200

    Clean up schema files

--- schema/dbx_pictures.sql
+++ schema/dbx_pictures.sql
@@ -1,24 +1,19 @@
 DROP TABLE IF EXISTS dbx_pictures;
 
 CREATE TABLE dbx_pictures (
-path CHAR(100),
-topic CHAR(30));
-
-ALTER TABLE dbx_pictures
-ADD taken datetime,
-ADD pic_id INT PRIMARY KEY AUTO_INCREMENT;
+	path CHAR(100),
+	topic CHAR(30),
+	taken datetime,
+	pic_id INT PRIMARY KEY AUTO_INCREMENT
+);
 
 CREATE TABLE faces (
-garg_id INT PRIMARY KEY,
-name char(30)
+	db_id INT PRIMARY KEY,
+	name char(30)
 );
-
-ALTER TABLE faces CHANGE garg_id db_id INT;
-
 
 CREATE TABLE dbx_pictures_faces (
-garg_id INT,
-pic_id INT
+	db_id INT,
+	pic_id INT
 );
 
-ALTER TABLE dbx_pictures_faces CHANGE `garg_id` `db_id` INT;
