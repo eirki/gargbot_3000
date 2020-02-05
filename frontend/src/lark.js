d1a4b2bf8931fe7546fa6d3040d131144e8a27c8
eirki <eirik.b.stavest@gmail.com>
2020-02-05 22:19:56 +0100

    Use parcel not webpack

--- frontend/src/lark.js
+++ frontend/src/lark.js
@@ -2,10 +2,40 @@
 
 import countdown from "countdown";
 
+console.log(countdown);
+
+function setBackground() {
+    const base_url = process.env.api_url
+    let args = process.env.countdown_args;
+    args = args.split(" ")
+    args = args.join(",")
+    let url = base_url + "/" + args
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+        .then(response => {
+            if (response.status !== 200) {
+                console.log("Fetch problem. Status Code:", response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            let image_url = data["url"];
+            document.body.style.background = `url(${image_url}) no-repeat center center`;
+        })
+        .catch(err => console.log('Fetch Error', err));
+}
+
+
 function startTimer() {
     const message = document.getElementById("message");
     const display = document.getElementById("time");
     const until_date = new Date(Number(process.env.countdown_date));
+    console.log(until_date)
     const countdown_message = process.env.countdown_message;
     const ongoing_message = process.env.ongoing_message;
     const finished_message = process.env.finished_message;
@@ -31,4 +61,6 @@ function startTimer() {
     }, 1000);
 }
 
+
+// setBackground()
 startTimer();
