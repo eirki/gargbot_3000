90c684c296b156d3ec4b69c5fc5a40a50ae7f034
eirki <eirik.b.stavest@gmail.com>
2021-01-25 22:13:45 +0100

    Use bootstrap for journeys page

--- frontend/src/journey.js
+++ frontend/src/journey.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import h from 'hyperscript'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { getBackend, getToken, redirectLogin } from "./utils.js"
 import * as config from "./config.js"
@@ -12,53 +13,67 @@ async function startStop(action, journey_id, token) {
 
 
 async function journeyList(token) {
+    let keys = ["journey_id", "destination", "origin", "started_at", "finished_at", "n_waypoints", "distance", "ongoing"]
     return await getBackend(token, "/list_journeys")
         .then(data => data["journeys"])
         .then(journeys => [
             h("h3", "Journeys"),
-            h("table", [
-                h("tr",
-                    Object.keys(journeys[0]).map(header => h("th", header))),
-                journeys.map(journey => h(
+            h("table.table", [
+                h("thead",
+                    [h("tr",
+                        [keys.map(header => h("th", header)),
+                        h("th", ""),
+                        h("th", "")])]),
+                h("tbody", journeys.map(journey => h(
                     "tr",
-                    Object.values(journey).map(val => h("td", val)),
-                    h("td", h("button", "start", { onclick: (() => startStop("start", journey['id'], token)) })),
-                    h("td", h("button", "stop", { onclick: (() => startStop("stop", journey['id'], token)) })),
-                ))
+                    keys.map(key => h("td", journey[key])),
+                    h("td", [h("button.btn.btn-primary", { type: "button" }, "start", { onclick: (() => startStop("start", journey['id'], token)) })]),
+                    h("td", h("button.btn.btn-warning", { type: "button" }, "stop", { onclick: (() => startStop("stop", journey['id'], token)) })),
+                )))
             ])]
         )
 }
 
-function uploadForm() {
+function uploadForm(token) {
     return [
         h("h3", "New journey"),
         h("form", { onsubmit: async e => await submitForm(e, token) }, [
-            h("input", {
-                type: "file",
-                required: true,
-                accept: ".gpx",
-                name: "uploadFile",
-
-            }),
             h("br"),
-            h("br"),
-            h("label", "Origin"),
-            h("br"),
-            h("input", {
-                type: "text",
-                required: true,
-                name: "origin",
-            }),
-            h("br"),
-            h("label", "Destination"),
-            h("br"),
-            h("input", {
-                type: "text",
-                required: true,
-                name: "dest",
-            }),
-            h("br"),
-            h("input", {
+            h("div.mb-3", [
+                h("input.form-control", {
+                    type: "file",
+                    required: true,
+                    accept: ".gpx",
+                    name: "uploadFile",
+                })
+            ]),
+            h("div.mb-3.row", [
+                h("label.col-sm-2.col-form-label",
+                    { htmlFor: "originName" },
+                    "Origin"
+                ),
+                h("div.col-sm-10", [
+                    h("input.form-control", {
+                        id: "originName",
+                        type: "text",
+                        required: true,
+                        name: "origin",
+                    })]),
+            ]),
+            h("div.mb-3.row", [
+                h("label.col-sm-2.col-form-label",
+                    { htmlFor: "destName" },
+                    "Dest"
+                ),
+                h("div.col-sm-10", [
+                    h("input.form-control", {
+                        id: "destName",
+                        type: "text",
+                        required: true,
+                        name: "dest",
+                    })]),
+            ]),
+            h("input.btn.btn-primary", {
                 type: "submit",
             })
         ])
@@ -68,9 +83,9 @@ function uploadForm() {
 
 async function submitForm(e, token) {
     e.preventDefault()
-    let file = e.target.uploadFile.files[0]
-    let origin = e.target.origin.value
-    let dest = e.target.dest.value
+    let file = e.target["uploadFile"].files[0]
+    let origin = e.target["origin"].value
+    let dest = e.target["dest"].value
     let formData = new FormData();
     formData.append('file', file)
     formData.append('origin', origin)
@@ -90,7 +105,7 @@ async function submitForm(e, token) {
 function updateButton(token) {
     return [
         h("h3", "Ongoing journey"),
-        h("button", "Run update", {
+        h("button.btn.btn-primary", "Run update", {
             onclick: (
                 async () => await getBackend(token, "/run_journey_update")
                     .then(response => alert(response.status)))
@@ -106,9 +121,11 @@ async function main() {
         redirectLogin("journey")
     }
     var root = document.body
-    root.appendChild(h("div", [
+    root.appendChild(h("div.ms-3", [
         await journeyList(token),
+        h("br"),
         uploadForm(token),
+        h("br"),
         updateButton(token)
     ]))
 }
